67d9d145592ac725797c807eb01f15d066f8a2db
Rob <rob.dawson@consensys.net>
2021-07-12 07:40:29 +1000

    Updated to match deployed version.

--- cb-sol-cli/deploy_usdc_erc20_synthetic.sh
+++ cb-sol-cli/deploy_usdc_erc20_synthetic.sh
@@ -1,6 +1,4 @@
-cb-sol-cli --url $RPC_URL --privateKey $PRIVATE_KEY --gasPrice 10000000000 deploy --erc20 \
-    --erc20Symbol USDC --erc20Name "USD Coin" --erc20Symbol USDC --erc20Decimals 6
 # set -o allexport; source .env; set +o allexport
 cb-sol-cli --url $RPC_URL --privateKey $PRIVATE_KEY \
- --gasPrice 10000000000 deploy --erc20 --erc20Symbol USDC \ 
+ --gasPrice 10000000000 deploy --erc20 --erc20Symbol USDC \
  --erc20Name "Pegged US Dollar Coin on Palm" --erc20Symbol pUSDC --erc20Decimals 6
